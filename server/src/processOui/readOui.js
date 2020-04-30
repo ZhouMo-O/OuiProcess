434fa57a40ae929560448e52f24bab21b6d5d005
xiaowu <1181649328@qq.com>
2020-04-30 17:26:35 +0800

    完成了oui文件的处理

--- server/src/processOui/readOui.js
+++ server/src/processOui/readOui.js
@@ -7,26 +7,26 @@ let fRead = fs.createReadStream(fReadName);
 
 var objReadline = readline.createInterface({
   input: fRead,
-  // 这是另一种复制方式，这样on('line')里就不必再调用fWrite.write(line)，当只是纯粹复制文件时推荐使用
-  // 但文件末尾会多算一次index计数   sodino.com
-  //  output: fWrite,
-  //  terminal: true
 });
 
 let OuiArr = [];
+
 objReadline.on("line", (line) => {
   let keyReg = /([0-9a-z/-][0-9a-z/-])-([0-9a-z/-][0-9a-z/-])-([0-9a-z/-][0-9a-z/-])/gi;
-  let incReg = /\s\s/;
+  let incReg = /\(hex\)\t\t[a-z\s,.]+/i;
 
   let ouiKey = keyReg.exec(line);
-  if (ouiKey !== null) {
+  let ouiInc = incReg.exec(line);
+
+  if (ouiKey !== null && ouiInc !== null) {
     let ouiObj = new Object();
     ouiObj.key = ouiKey[0];
+    ouiObj.inc = ouiInc[0].slice(7);
+    OuiArr.push(ouiObj);
   }
-
-  console.log(incReg.exec(line));
 });
 
 objReadline.on("close", () => {
+  console.log(OuiArr);
   console.log("readline close...");
 });
